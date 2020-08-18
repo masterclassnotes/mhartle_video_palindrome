1ef2046fcd190baf27b54b096eb5f2788535f11e
Zhong Gu <guzhong1994@gmail.com>
2020-08-18 12:10:44 +0800

    Initialize repository

---
+++ lib/mhartle_video_palindrome.rb
@@ -0,0 +1,6 @@
+require "mhartle_video_palindrome/version"
+
+module MhartleVideoPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mhartle_video_palindrome/version.rb
@@ -0,0 +1,3 @@
+module MhartleVideoPalindrome
+  VERSION = "0.1.0"
+end
